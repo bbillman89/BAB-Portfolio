792aa7020881793399362c3212d5c000b27b5a03
Mtnman <billman.designs@gmail.com>
2018-12-22 11:23:16 -0500

    smooth scrolling working and social media added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,23 @@
+/*
+
+Brett Billman
+Portfolio site
+main.js
+December 2018
+
+*/
+
+
 $(document).ready(function(){
+
+    //Smooth Scroll to selected anchor
     $( "a.scrollLink" ).click(function( event ) {
+
         event.preventDefault();
+        
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+
     });
+
+
 });
