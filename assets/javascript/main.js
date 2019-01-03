f95051a75bee4b6594e1692a41463b96b1552ab4
Mtnman <billman.designs@gmail.com>
2019-01-03 16:01:21 -0500

    before class

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,14 +10,6 @@ December 2018
 
 $(document).ready(function(){
 
-    //Smooth Scroll to selected anchor
-    $( "a.scrollLink" ).click(function( event ) {
-
-        event.preventDefault();
-        
-        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
-
-    });
 
 
 });
